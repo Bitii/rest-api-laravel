5683b73898aa295330c3d1db3cc2b20b3cf923ed
Bitii <tari.tibor70@gmail.com>
2024-09-24 17:18:57 +0200

    product patch done

--- laravel-rest-api/app/Http/Controllers/ProductsController.php
+++ laravel-rest-api/app/Http/Controllers/ProductsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Http\Requests\ProductRequest;
 
 class ProductsController extends Controller
 {
@@ -13,19 +13,16 @@ class ProductsController extends Controller
         return response()->json(['products' => $products]);
     }
 
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        $request->validate([
-            'category' => 'required|string|min:2',
-            'name' => 'required|string|min:3',
-            'description' => 'required|string|min:10',
-            'picture' => 'required|string|min:20',
-            'price' => 'required|numeric',
-            'stock' => 'nullable|numeric',
-            
-        ]);
         $product = Product::create($request->all());
-        
+        return response()->json(['product' => $product]);
+    }
+
+    public function update(ProductRequest $request, $id)
+    {    
+        $product = Product::findOfFail($id);
+        $product->update($request->all());
         return response()->json(['product' => $product]);
     }
 }
--- laravel-rest-api/routes/api.php
+++ laravel-rest-api/routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\ProductsController;
 Route::get('/products', [ProductsController::class, 'index']);
 Route::post('/products', [ProductsController::class, 'store'])
     ->middleware('auth:sanctum');
+Route::patch('/products/{id}', [ProductsController::class, 'update'])
+    ->middleware('auth:sanctum');
 Route::post('/users/login', [UsersControllers::class, 'login']);
 Route::get('/users', [UsersControllers::class, 'index'])
     ->middleware('auth:sanctum');
