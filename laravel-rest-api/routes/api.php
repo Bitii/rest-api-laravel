329eea3eb7a2073b5019ff5c6e2a56a494c2d013
Bitii <tari.tibor70@gmail.com>
2024-09-24 17:02:39 +0200

    get all products

--- laravel-rest-api/routes/api.php
+++ laravel-rest-api/routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\UsersControllers;
 use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
+Route::get('/products', [ProductsController::class, 'index']);
 Route::post('/users/login', [UsersControllers::class, 'login']);
-
 Route::get('/users', [UsersControllers::class, 'index'])->middleware('auth:sanctum');
