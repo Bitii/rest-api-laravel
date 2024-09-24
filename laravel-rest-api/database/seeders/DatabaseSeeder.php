b13a23f9e8e28a6ae3db3f6c0f6d54fe48caf746
Bitii <tari.tibor70@gmail.com>
2024-09-24 16:15:22 +0200

    user controllers - user token

--- laravel-rest-api/database/seeders/DatabaseSeeder.php
+++ laravel-rest-api/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Tibi',
+            'email' => 'tibi@example.com',
+            'password' => '123',
         ]);
     }
 }
