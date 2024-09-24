13e1007061e4265a02d57171f4f7cc784fd6512d
Bitii <tari.tibor70@gmail.com>
2024-09-24 17:22:41 +0200

    product delete

--- laravel-rest-api/app/Http/Controllers/ProductsController.php
+++ laravel-rest-api/app/Http/Controllers/ProductsController.php
@@ -25,4 +25,12 @@ class ProductsController extends Controller
         $product->update($request->all());
         return response()->json(['product' => $product]);
     }
+
+    public function destroy($id)
+    {
+        $product = Product::findOfFail($id);
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully',
+        'id' => $id]);
+    }
 }
--- laravel-rest-api/routes/api.php
+++ laravel-rest-api/routes/api.php
@@ -7,10 +7,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
 
 Route::get('/products', [ProductsController::class, 'index']);
+
 Route::post('/products', [ProductsController::class, 'store'])
     ->middleware('auth:sanctum');
+
 Route::patch('/products/{id}', [ProductsController::class, 'update'])
     ->middleware('auth:sanctum');
+
+Route::delete('/products/{id}', [ProductsController::class, 'destroy'])
+    ->middleware('auth:sanctum');
+
 Route::post('/users/login', [UsersControllers::class, 'login']);
+
 Route::get('/users', [UsersControllers::class, 'index'])
     ->middleware('auth:sanctum');
